1df40927c01ec3fa13b1eb8957c71af87223619a
abhiverse01 <www.abhishekshah007@gmail.com>
2024-07-26 13:44:01 +0545

    Llama3-Chat v2.6

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { FaCog, FaBell, FaSun, FaMoon, FaArrowLeft, FaArrowRight } from 'react-icons/fa';  // Using different icons for testing
 
 export default function Home() {
